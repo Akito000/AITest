f3c873183effc82d273c776299ce24bdfae13e58
Akito000 <akito02162004@softbank.ne.jp>
2026-04-16 19:03:02 +0900

    a

--- GoalBase/SubGoal.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// サブゴールの基底クラス
-/// Execute が true を返したら、そのサブゴールは完了
-/// </summary>
-public abstract class SubGoal
-{
-    public abstract bool Execute(EnemyMoveAgent owner, EnemyContext context, EnemyMemory memory);
-}
